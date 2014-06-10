45f479f934e3ee36f2d109b12e91ba192b116600
Ryan Skeele <rycoske@gmail.com>
2014-06-10 12:37:59 -0700

    random burning

--- wildfire.py
+++ wildfire.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import mpl_toolkits.mplot3d.axes3d as axes3d
 import random
+import matplotlib.animation as animation
+from copy import copy, deepcopy
+from noise import pnoise1
 
 i=0
 
@@ -15,7 +18,7 @@ i=0
 while (i <= 3):
 
 	fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
-	N = 100
+	N = 10
 	X, Y = np.meshgrid(range(N), range(N))
 	heights = 0 #np.sin(np.pi*np.sqrt(X**2+Y**2)/N)+np.cos(np.sqrt(X**2+Y**2))
 	
@@ -36,7 +39,7 @@ while (i <= 3):
 	print t1
 	print t2
 	colors[t1][t2] = 'r'
-
+	burning=deepcopy(colors)
 			
 	#for y in range(N):
 	#	for x in range(N):
@@ -48,8 +51,44 @@ while (i <= 3):
 	
 
 
-	ax.plot_surface(X, Y, heights, rstride=1, cstride=1, facecolors= colors)
-	plt.show()
+	terrain= ax.plot_surface(X, Y, heights, rstride=1, cstride=1, facecolors= colors)
+	#plt.show()
 	#print colors
 	i=4
 
+def update(i, ax, fig):
+	global colors
+	global N
+	ax.cla()
+	
+	for y in range(N):
+		for x in range(N):
+			if colors[x][y] is 'r':
+				values= [0, 1, -1]
+				
+				k=x+random.choice(values)
+				l=y+random.choice(values)
+				if k >= 0 and l >= 0 and k < N and l < N:
+					burning[k][l]= 'r' 
+				
+				#burning[x+1][y-1]= 'r'
+				#burning[x-1][y+1]= 'r'
+				#burning[x-1][y-1]= 'r'
+				#burning[x+1][y+1]= 'r'
+				#burning[x+1][y]= 'r'
+				#burning[x][y+1]= 'r'
+				#burning[x-1][y]= 'r'
+				#burning[x][y-1]= 'r'
+
+			else:
+				colors[x][y]= colors[x][y]
+	
+
+	colors=deepcopy(burning)	
+	terrain= ax.plot_surface(X, Y, heights, rstride=1, cstride=1, facecolors= colors)
+	return terrain,
+
+ani = animation.FuncAnimation(fig, update,
+	frames= xrange(100),
+	fargs=(ax, fig), interval=10)
+plt.show()
